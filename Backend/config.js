bb91377bfc528af6361ee03c3c6e494a3d314536
Zaid <zaidalqadiri@gmail.com>
2024-12-17 22:43:25 -0700

    created connection to express server and connected to a mongoDB database

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://zaidalqadiri:ZaiD2oo2@book-store-project.7eazn.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Project'
--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose'
 
-const app = express();
+const app = express();  // create a web server using express
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port ${PORT}`)
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("Welcome")
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {  // successfull connection to databsed
+        console.log('APP connected to database');
+        // run the express server only if the database connection is successful
+        app.listen(PORT, () => {
+            console.log(`App is listening to port ${PORT}`)
+        });
+    })
+
+    .catch((error) => {  // used for catching errors and then printing the error
+        console.log(error);
+    });
