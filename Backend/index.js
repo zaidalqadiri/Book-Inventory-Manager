5903350ef681a72f1a9c304528bf5b372d191504
Zaid <zaidalqadiri@gmail.com>
2024-12-17 23:45:53 -0700

    created book model and created a route for saving a book

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,41 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose'
+import { Book } from './models/bookModel.js';
 
 const app = express();  // create a web server using express
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome")
 });
+
+// Route for saving a new book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return reponse.status(400).send({
+                message: 'Send all required fields: title, author, publishYear', 
+            });
+        }
+        const newBook = {
+            title: request.body.title, 
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook)
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message })
+    }
+});  // post method is used to create a new resource
 
 mongoose
     .connect(mongoDBURL)
