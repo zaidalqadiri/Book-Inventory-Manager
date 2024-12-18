8b4f6066849e05a52c01fbbdd31388beb445d6ef
Zaid <zaidalqadiri@gmail.com>
2024-12-18 15:36:15 -0700

    implemented route for deleting a book

--- Backend/index.js
+++ Backend/index.js
@@ -90,9 +90,27 @@ app.put('/books/:id', async (request, response) => {
         return response.status(200).send({ message: 'Book updated successfully' })
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({ message: error.message})
+        response.status(500).send({ message: error.message })
     }
 });
+
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+
+        return response.status(200).send({ message: 'Book deleted successfully' })
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
