e0f11468302d915c2c9e232e45a72991fecd3ea7
Zaid <zaidalqadiri@gmail.com>
2024-12-18 15:23:12 -0700

    implemented route for updating a book

--- Backend/index.js
+++ Backend/index.js
@@ -58,7 +58,7 @@ app.get('/books/:id', async (request, response) => {
     try {
         const { id } = request.params;
 
-        const book = await Book.findbyId(id);
+        const book = await Book.findById(id);
         return response.status(200).json(book);
 
     } catch(error) {
@@ -67,6 +67,32 @@ app.get('/books/:id', async (request, response) => {
     }
 });
 
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear 
+        ) {
+            return response.status(400).send({
+                message: 'Send all required field: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' })
+        }
+
+        return response.status(200).send({ message: 'Book updated successfully' })
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message})
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
