2b6ba19f41864775089f25d3461d91fd236e5fcc
Zaid <zaidalqadiri@gmail.com>
2024-12-19 14:43:31 -0700

    implemented back button componenet

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -76,8 +76,7 @@ const Home = () => {
                             </tr>
                         ))}
                     </tbody>
-
-
+                    
                 </table>
             )}
         </div>
