7d8b0043998dbce3b9aaac0f1213f63c32b12408
Zaid <zaidalqadiri@gmail.com>
2024-12-19 23:34:42 -0700

    implemented the delete book page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
             {loading ? (
                 <Spinner />
             ) : (
-                <table className='w-full border-seperate border-spacing-2'>
+                <table className='w-full border-separate border-spacing-2'>
                     <thead>
                         <tr>
                             <th className='border border-slate-600 rounded-md'>No</th>
